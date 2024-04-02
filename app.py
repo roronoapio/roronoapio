0fe89b4970c7935d209480f6a85122c438c73da9
pio <147454593+roronoapio@users.noreply.github.com>
2024-04-02 09:52:31 +0700

    Update app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+ptint ("## program phyton menghitung luas segitiga")
+pint ("===========================================")
+print ()
+
+def hitungluassegitiga(a,t):
+    retutm round (0.5 * a * t,2)
+
+    alas = float(input(input alas segitiga))
+    tinggi = float(input(input tinggi segitiga))
+    print('luas segitiga= ', hitungluassegitiga(alas,tinggi))
+    print()
